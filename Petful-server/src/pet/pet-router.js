b1acebd53a89693531fb3e2b75d80c2d89ad4206
David Bolin <david@bolin.at>
2019-05-24 11:02:36 -0700

    fixed delete and get endpoints

--- Petful-server/src/pet/pet-router.js
+++ Petful-server/src/pet/pet-router.js
@@ -5,22 +5,22 @@ const {dogs, cats} = require('../dataset');
 
 petRouter
   .get('/cat', (req, res, next) => {
-    res.status(200).json(cats.first.value)
-      .catch(next);
+    return res.status(200).json(cats.first.value);
   } )
   .get('/dog', (req, res, next) => {
-    res.status(200).json(dogs.first.value)
-      .catch(next);
+    return res.status(200).json(dogs.first.value);
   })
   .delete('/cat', (req, res, next) => {
     // so that we won't run out of cats, the operation will actually
     // just move the cat to the back of the line 
     cats.enqueue(cats.dequeue());
+    return res.status(204).end();
 
   })
   .delete('/dog', (req, res, next) => {
     // same with dog
     dogs.enqueue(dogs.dequeue());
+    return res.status(204).end();
   });
 
 
